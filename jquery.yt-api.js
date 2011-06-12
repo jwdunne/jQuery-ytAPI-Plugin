be559e70aeff079a1269765129361a1f7f520b0d
James <me@jameswdunne.com>
2011-06-12 20:31:18 +0100

    Done some JSLintage. Crazy how bad at JS I am

--- jquery.yt-api.js
+++ jquery.yt-api.js
@@ -1,17 +1,30 @@
 var YtApi;
 (function ($) {
   "use strict";
+  
+  /**
+   * Build a YouTube Data API request for a video.
+   * @param string videoId The video ID
+   & @return string The API request URL
+   */
+  function buildApiRequest(videoId) {
+    return 'http://gdata.youtube.com/feeds/api/videos/'
+      + videoId
+      + '?v=2&alt=jsonc';
+  }
+  
   YtApi = function (url) {
-    var $this = this;
-    
-    // Need to chop out the video ID with a bit of regex.
-    var videoId = url.match(/watch\?v=(.*)/)[1];
+    // 2 things:
+    // 1. Need to store a reference to this object.
+    // 2. Need to chop out the video ID with a bit of regex.
+    var $this   = this,
+      videoId = url.match(/watch\?v=([A-Za-z0-9\-_]+)/)[1];
     
     // Make request to YouTube Data API
     $.ajax(
       buildApiRequest(videoId),
       {
-        success: function (response, status) {
+        success: function (response) {
           $this.get = function (attr) {
             return response.data[attr];
           };
@@ -22,10 +35,4 @@ var YtApi;
       }
     );
   };
-  
-  function buildApiRequest(videoId) {
-    return 'http://gdata.youtube.com/feeds/api/videos/'
-           + videoId
-           + '?v=2&alt=jsonc';
-  }
-})(jQuery)
+}(jQuery));
